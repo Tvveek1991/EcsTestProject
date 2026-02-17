72602ffd43c00aadb0809ccba380fcd9ec308203
emmaa <emmaalla1991@gmail.com>
2026-02-17 22:35:41 +0700

    add coin animation

---
+++ Assets/Project/Scripts/Gameplay/Systems/CoinsViewAnimationSystem.cs
@@ -0,0 +1,58 @@
+using DG.Tweening;
+using Leopotam.EcsLite;
+using Project.Scripts.Gameplay.Components;
+using UnityEngine;
+
+namespace Project.Scripts.Gameplay.Systems
+{
+    public class CoinsViewAnimationSystem : IEcsInitSystem, IEcsRunSystem
+    {
+        private EcsWorld m_world;
+
+        private EcsFilter m_animatedCoinViewFilter;
+        
+        private EcsPool<TransformComponent> m_transformPool;
+        private EcsPool<CoinsCounterChangeComponent> m_coinsCounterChangePool;
+        private EcsPool<CoinViewFlyAwayAnimationComponent> m_coinViewFlyAwayAnimationPool;
+
+        public void Init(IEcsSystems systems)
+        {
+            m_world = systems.GetWorld();
+        
+            m_animatedCoinViewFilter = m_world.Filter<CoinViewRefComponent>().Inc<CoinViewFlyAwayAnimationComponent>().Inc<TransformComponent>().End();
+
+            m_transformPool = m_world.GetPool<TransformComponent>();
+            m_coinsCounterChangePool = m_world.GetPool<CoinsCounterChangeComponent>();
+            m_coinViewFlyAwayAnimationPool = m_world.GetPool<CoinViewFlyAwayAnimationComponent>();
+        }
+
+        public void Run(IEcsSystems systems)
+        {
+            foreach (var coinView in m_animatedCoinViewFilter)
+            {
+                var coinTransform = m_transformPool.Get(coinView).ObjectTransform;
+                
+                m_coinViewFlyAwayAnimationPool.Del(coinView);
+                
+                var sequence = DOTween.Sequence();
+                sequence
+                    .Append(coinTransform.DOScale(0, .5f))
+                    .Join(coinTransform.DOLocalMoveY(coinTransform.localPosition.y + 1, .5f))
+                    .OnComplete(() =>
+                    {
+                        var entity = m_world.NewEntity();
+                        m_coinsCounterChangePool.Add(entity).CorrectionValue = 1;
+
+                        coinTransform.DOKill();
+                        
+                        sequence.Kill();
+                        sequence = null;
+                    
+                        m_world.DelEntity(coinView);
+                        
+                        Object.Destroy(coinTransform.gameObject);
+                    });
+            }
+        }
+    }
+}
--- Assets/Project/Scripts/Gameplay/Systems/CoinsViewCheckSystem.cs
+++ Assets/Project/Scripts/Gameplay/Systems/CoinsViewCheckSystem.cs
@@ -1,6 +1,5 @@
 using Leopotam.EcsLite;
 using Project.Scripts.Gameplay.Components;
-using UnityEngine;
 
 namespace Project.Scripts.Gameplay.Systems
 {
@@ -9,11 +8,9 @@ namespace Project.Scripts.Gameplay.Systems
         private EcsWorld m_world;
 
         private EcsFilter m_coinViewRefFilter;
-        private EcsFilter m_gameLevelViewRefsFilter;
 
         private EcsPool<CoinViewRefComponent> m_coinViewRefPool;
-        private EcsPool<GameLevelViewRefComponent> m_gameLevelViewRefsPool;
-        private EcsPool<CoinsCounterChangeComponent> m_coinsCounterChangePool;
+        private EcsPool<CoinViewFlyAwayAnimationComponent> m_coinViewFlyAwayAnimationPool;
 
 
         public void Init(IEcsSystems systems)
@@ -23,7 +20,7 @@ namespace Project.Scripts.Gameplay.Systems
             m_coinViewRefFilter = m_world.Filter<CoinViewRefComponent>().End();
             
             m_coinViewRefPool = m_world.GetPool<CoinViewRefComponent>();
-            m_coinsCounterChangePool = m_world.GetPool<CoinsCounterChangeComponent>();
+            m_coinViewFlyAwayAnimationPool = m_world.GetPool<CoinViewFlyAwayAnimationComponent>();
         }
 
         public void Run(IEcsSystems systems)
@@ -32,17 +29,10 @@ namespace Project.Scripts.Gameplay.Systems
             {
                 if (m_coinViewRefPool.Get(coinView).CoinView.Sensor.IsConnected)
                 {
-                    var entity = m_world.NewEntity();
-                    m_coinsCounterChangePool.Add(entity).CorrectionValue = 1;
+                    m_coinViewFlyAwayAnimationPool.Add(coinView);
                     
                     m_coinViewRefPool.Get(coinView).CoinView.Sensor.Disable(float.MaxValue);
-
-                    ref var view = ref m_coinViewRefPool.Get(coinView);
-                    Object.Destroy(view.CoinView.gameObject);
-                    
-                    m_world.DelEntity(coinView);
                 }
-                
             }
         }
     }
